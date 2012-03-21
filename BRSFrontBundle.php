2a49a2346b6739adee392cd5e64fc0771dae52c2
Sam Mateosian <sam@bigroomstudios.com>
2012-03-21 13:16:57 -0400

    creating FrontBundle repo

---
+++ BRSFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BRS\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BRSFrontBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BRS\FrontBundle\Controller;
+
+use BRS\CoreBundle\Core\Utility;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * DefaultController defines routes for front end content delivery
+ * @Route("")
+ */
+class DefaultController extends Controller
+{
+	
+	/**
+	 * Displays a form to create a new entity for this admin module
+	 *
+	 * @Route("/")
+	 * @Template("BRSFrontBundle:Default:index.html.twig")
+	 */
+	public function indexAction()
+	{
+		return array('title' => 'Hello World');
+	}
+}
